1ebc47498c14e580c79c5c2b47e6d6f33fd1a9cf
Joaci Queiroz <jcqroz@gmail.com>
2024-07-26 22:38:42 -0300

    Initial commit - feat: Criação da estrutura Spring Boot

---
+++ src/main/java/web/Qroz/SantanderDevJava2024Application.java
@@ -0,0 +1,13 @@
+package web.Qroz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevJava2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevJava2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/web/Qroz/SantanderDevJava2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package web.Qroz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevJava2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
